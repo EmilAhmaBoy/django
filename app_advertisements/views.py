2b4cad7ad9e73912f482dd337db151853ca6f857
EmilAhmaBoy <emilahmaboy@gmail.com>
2023-07-22 12:05:47 +0300

    Добавлены все нужные страницы

--- app_advertisements/views.py
+++ app_advertisements/views.py
@@ -9,4 +9,24 @@ def index(request):
 def top_sellers(request):
     return render(request, 'top-sellers.html')
 
+
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def advertisement(request):
+    return render(request, 'advertisement.html')
+
 # Create your views here.
